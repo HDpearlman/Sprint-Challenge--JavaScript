55f9773350c391344a17ffc45f45627f30d1e984
HDpearlman <HDpearlman@users.noreply.github.com>
2020-02-22 12:29:57 -0500

    finished dinosaurs in objects-arrays. created university array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,55 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const dinoTyranno = {
+  name:'Tyrannosaurus',
+  diet:'carnivorous',
+  weight: 7000 + `kg`,
+  length: 12 + 'm',
+  period: 'Late Cretaceous'
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const dinoStego = {
+  name:'Stegosaurus',
+  diet:'herbivorous',
+  weight: 2000 + `kg`,
+  length: 9 + 'm',
+  period: 'Late Jurassic'
+}
+
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const dinoVeloci = {
+  name:'Velociraptor',
+  diet:'carnivorous',
+  weight: 15 + `kg`,
+  length: 1.8 + 'm',
+  period: 'Late Cretaceous'
+}
+
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinoTyranno.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinoVeloci.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinoStego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinoTyranno.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+dinoTyranno.roar = () => "RAWERSRARARWERSARARARRRR!";
+
+console.log(dinoTyranno.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +78,11 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+
+
+
+const universities = graduates.map(uni => uni.university).sort();
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
